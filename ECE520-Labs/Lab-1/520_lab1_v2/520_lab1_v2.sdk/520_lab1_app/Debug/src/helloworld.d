6c0d9f672737978391710a24a7cf38c58c3957c2
aaronically <48500518+aaronically@users.noreply.github.com>
2021-03-01 20:22:37 -0800

    Project files for ZedBoard

---
+++ ECE520-Labs/Lab-1/520_lab1_v2/520_lab1_v2.sdk/520_lab1_app/Debug/src/helloworld.d
@@ -0,0 +1,51 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../520_lab1_app_bsp/ps7_cortexa9_0/include/xgpio.h \
+ ../../520_lab1_app_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../520_lab1_app_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../520_lab1_app_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../520_lab1_app_bsp/ps7_cortexa9_0/include/xgpio_l.h \
+ ../../520_lab1_app_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../520_lab1_app_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../520_lab1_app_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../520_lab1_app_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../520_lab1_app_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../520_lab1_app_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../520_lab1_app_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../520_lab1_app_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../520_lab1_app_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../520_lab1_app_bsp/ps7_cortexa9_0/include/xil_printf.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../520_lab1_app_bsp/ps7_cortexa9_0/include/xgpio.h:
+
+../../520_lab1_app_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../520_lab1_app_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../520_lab1_app_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../520_lab1_app_bsp/ps7_cortexa9_0/include/xgpio_l.h:
+
+../../520_lab1_app_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../520_lab1_app_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../520_lab1_app_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../520_lab1_app_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../520_lab1_app_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../520_lab1_app_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../520_lab1_app_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../520_lab1_app_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../520_lab1_app_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../520_lab1_app_bsp/ps7_cortexa9_0/include/xil_printf.h:
---
+++ ECE520-Labs/Lab-1/520_lab1_v2/520_lab1_v2.sdk/520_lab1_app/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../520_lab1_app_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../520_lab1_app_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../520_lab1_app_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../520_lab1_app_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../520_lab1_app_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../520_lab1_app_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../520_lab1_app_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../520_lab1_app_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
